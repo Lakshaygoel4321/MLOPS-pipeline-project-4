28c027e505f74712cc87d559fb5940a35c078cde
Lakshaygoel4321 <iamlakshaygoel5990@gmail.com>
2025-03-03 13:38:43 +0530

    adding the model trainer file

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_ingestion import DataIngestionConfig
 from src.components.data_transformation import DataTransformer,DataTransformerConfig
+from src.components.model_trainer import ModelTrainer,ModelTrainerConfig
 from src.logger import logging
 from src.exception import CustomException
 import sys
@@ -13,9 +14,11 @@ if __name__ == "__main__":
         train_array,test_array = obj.initiate_data_ingestion()
 
         data_transformer = DataTransformer()
-        data_transformer.initiate_data_transformer(train_array,test_array)
+        train_file_array,test_file_array,_ = data_transformer.initiate_data_transformer(train_array,test_array)
+
+        model_trainer = ModelTrainer()
+        model_trainer.initiate_model_trainer(train_file_array,test_file_array)
 
     except Exception as e:
         raise CustomException(e,sys)
-    
     
